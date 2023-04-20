49af92913e000f8c913dcb77004daba7f0d29d26
maximboolbool <remezmax117@gmail.com>
2023-04-20 20:19:22 +0300

    delete some support methods

--- MyNotes/Program.cs
+++ MyNotes/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.IdentityModel.Tokens;
+using MyNotes;
 using MyNotes.Context;
+using MyNotes.Services.IdentytiUserServices;
 using MyNotes.Services.NotesWorkerServices;
 using MyNotes.Services.UserAcountWorkersServices;
 
@@ -9,16 +13,31 @@ var builder = WebApplication.CreateBuilder();
 
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationContext>(opt=>opt.UseNpgsql(connection));
 builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+           ValidateIssuer  = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDINCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 builder.Services.AddTransient<IUserAcountWorker, DefaultUserAcountWorkerService>();
 builder.Services.AddTransient<INotesWorkerServices, DefaultNotesWorkerServices>();
+builder.Services.AddTransient<IIdentityUserService, DefaultIdentityService>();
+
 
 var app = builder.Build();
-
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthentication();
 app.UseAuthorization();
 
